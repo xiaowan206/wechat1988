1fe82011c68a833ba268992d67573c0ea63b85fd
wanzheng <wanzheng19880130@163.com>
2019-11-19 16:18:28 +0800

    init

---
+++ src/Wechat/Test/Test.php
@@ -0,0 +1,15 @@
+<?php
+namespace Wechat\Test;
+/**
+ * Created by PhpStorm.
+ * User: mozheng
+ * Date: 2019/11/19
+ * Time: 下午4:15
+ */
+class Test
+{
+    public function test()
+    {
+        echo 1111;
+    }
+}
